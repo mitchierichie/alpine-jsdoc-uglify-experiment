70b3d84ff06a41c94613c985ee5276bf77e279e8
mitchierichie <mitch@m.io>
2024-04-15 19:59:59 -0500

    add npm ci to build script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+npm ci
 rm -rf dist && mkdir dist && tsc && uglifyjs index.js --output dist/index.min.js && sed s/index\.js/index\.min\.js/ index.html > dist/index.html
 cp alpinejs.min.js dist/alpinejs.min.js
