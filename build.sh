2c27e54d7cfd272ec17fb90032dc8464ba25a3a6
mitchierichie <mitch@m.io>
2024-04-15 20:05:35 -0500

    fix bin path

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 npm ci
-rm -rf dist && mkdir dist && ./node_modules/bin/tsc && ./node_modules/bin/uglifyjs index.js --output dist/index.min.js && sed s/index\.js/index\.min\.js/ index.html > dist/index.html
+rm -rf dist && mkdir dist && ./node_modules/.bin/tsc && ./node_modules/.bin/uglifyjs index.js --output dist/index.min.js && sed s/index\.js/index\.min\.js/ index.html > dist/index.html
 cp alpinejs.min.js dist/alpinejs.min.js
